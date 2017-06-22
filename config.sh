77b9d1b6b502ee9e789c5379d0c44a18478937ce
muink <hukk1996@gmail.com>
2017-06-22 19:02:25 +0800

    add v1

--- config.sh
+++ config.sh
@@ -27,7 +27,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=magisk-luoli-font-nougat
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -50,7 +50,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "   Magisk Luoli Font(Nougat)   "
   ui_print "*******************************"
 }
 
