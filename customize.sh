a36567018b8b8d0a9eb865f3fdb7cd0598a7c6af
Anya Lin <hukk1996@gmail.com>
2025-08-09 22:11:24 +0800

    improvements

--- customize.sh
+++ customize.sh
@@ -11,9 +11,8 @@ unzip -o "$ZIPFILE" -x 'META-INF/*' fonts.tar.xz -d $MODPATH >&2
 # --------------------------------------------
 ui_print "- Searching in fonts.xml"
 [[ -d /sbin/.magisk/mirror ]] && MIRRORPATH=/sbin/.magisk/mirror || unset MIRRORPATH
-FILEPATH=/system/etc
-FILE=fonts.xml
-mkdir -p $MODPATH/$FILEPATH 2>/dev/null
+FILEPATH=/system/etc/fonts.xml
+mkdir -p $MODPATH/$(dirname $FILEPATH) 2>/dev/null
 
 ui_print "- Unzipping font files..."
 FONTSPATH=/system/fonts
@@ -24,29 +23,29 @@ tar -xf $TMPDIR/fonts.tar.xz -C $MODPATH/$FONTSPATH 2>/dev/null
 
 ui_print "- Installing fonts..."
 # CJK=(zh-Hans zh-Hant,zh-Bopo ja ko)
-RAWFONTS=$(sed -En '/<family name="sans-serif">/,/<\/family>/ {s|.*<font weight="400" style="normal">(.*)-Regular.ttf<\/font>.*|\1|p}' $MIRRORPATH/$FILEPATH/$FILE|cut -f1 -d-)
-NEWFONTS='Loli'
+TARGET=$(sed -En '/<family name="sans-serif">/,/<\/family>/ {s|.*<font weight="400" style="normal">(.*)-Regular.ttf<\/font>.*|\1|p}' $MIRRORPATH/$FILEPATH|cut -f1 -d-)
+SOURCE='Loli'
 SANS_CJK=LoliCJK-Regular.ttf
 
 # Just replace
-for _font in `ls -1 $MODPATH/$FONTSPATH/*.ttf | sed -Ene 's|.*/([^/]+)|\1|' -e "s|${NEWFONTS}-||p"`; do
-  ln -s $FONTSPATH/${NEWFONTS}-$_font $MODPATH/$FONTSPATH/${RAWFONTS}-$_font
+for _font in `ls -1 $MODPATH/$FONTSPATH/*.ttf | sed -Ene 's|.*/([^/]+)|\1|' -e "s|${SOURCE}-||p"`; do
+  ln -s ${SOURCE}-$_font $MODPATH/$FONTSPATH/${TARGET}-$_font
 done
 
 # With fonts.xml
-cp -af $MIRRORPATH/$FILEPATH/$FILE $MODPATH/$FILEPATH/$FILE 2>/dev/null
+cp -af $MIRRORPATH/$FILEPATH $MODPATH/$FILEPATH 2>/dev/null
 
-if [ -f "$MODPATH/$FILEPATH/$FILE" ]; then
+if [ -f "$MODPATH/$FILEPATH" ]; then
   # SansAll    sed -n "/^    <family name=\"sans-serif\">/,/<\/family>/p" fonts.xml
   # sed -i "/^    <family name=\"sans-serif\">/,/<\/family>/ {\
-  #   s|${RAWFONTS}-|${NEWFONTS}-|}" \
-  # $MODPATH/$FILEPATH/$FILE
+  #   s|${TARGET}-|${SOURCE}-|}" \
+  # $MODPATH/$FILEPATH
 
   # SansRegular
   # sed -i "/^    <family name=\"sans-serif\">/,/<\/family>/ {/weight=\"400\"/ {\
   #   s|style=\"normal\".*</font>$|style=\"normal\">$SANS</font>|; \
   #   s|style=\"italic\".*</font>$|style=\"italic\">${SANS-ITALIC}</font>|}}" \
-  # $MODPATH/$FILEPATH/$FILE
+  # $MODPATH/$FILEPATH
 
   # sed -n "/^    <family lang=\"ko\">$/,+1p" fonts.xml
   # CJK Sans    sed -n "/^    <family lang=\"ko\">/{n; p;}" fonts.xml
@@ -55,8 +54,8 @@ if [ -f "$MODPATH/$FILEPATH/$FILE" ]; then
   for _lang in zh-Hans zh-Hant,zh-Bopo ja ko; do
     sed -i "/^    <family lang=\"$_lang\">/{n; \
       s|weight=\"400\" style=\"normal\".*</font>$|weight=\"400\" style=\"normal\">$SANS_CJK</font>|}" \
-    $MODPATH/$FILEPATH/$FILE
-    # sed "/^    <family lang=\"$_lang\">/,/<\/family>/ c\    <family lang=\"$_lang\">\n        <font weight=\"400\" style=\"normal\">$SANS_CJK<\/font>\n        <font weight=\"400\" style=\"normal\" fallbackFor=\"serif\">$SANS_CJK<\/font>\n    <\/family>" $MODPATH/$FILEPATH/$FILE
+    $MODPATH/$FILEPATH
+    # sed "/^    <family lang=\"$_lang\">/,/<\/family>/ c\    <family lang=\"$_lang\">\n        <font weight=\"400\" style=\"normal\">$SANS_CJK<\/font>\n        <font weight=\"400\" style=\"normal\" fallbackFor=\"serif\">$SANS_CJK<\/font>\n    <\/family>" $MODPATH/$FILEPATH
   done
 fi
 
