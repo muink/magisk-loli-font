9e6905ae7162995f51229d181a4bf32ba5843e90
Anya Lin <hukk1996@gmail.com>
2025-09-07 17:04:54 +0800

    fix oneplus

--- action.sh
+++ action.sh
@@ -3,6 +3,7 @@
 MODDIR=${0%/*}
 
 SOURCE='Loli'
+SANS_CJK='LoliCJK-Regular.ttf'
 
 sleep_pause() {
     # APatch and KernelSU needs this
@@ -19,6 +20,11 @@ for _t in *.placeholder; do
     /system/bin/ln -sf ${SOURCE}-${_v#*-} ${_t}
     mv ${_t} ${_v}
 done
+
+# Oneplus
+[ ! -f "/system/fonts/SysSans-En-Regular.ttf" ] || /system/bin/ln -sf ${SOURCE}-Regular.ttf SysSans-En-Regular.ttf
+[ ! -f "/system/fonts/SysSans-Hans-Regular.ttf" ] || /system/bin/ln -sf ${SANS_CJK} SysSans-Hans-Regular.ttf
+[ ! -f "/system/fonts/SysSans-Hant-Regular.ttf" ] || /system/bin/ln -sf ${SANS_CJK} SysSans-Hant-Regular.ttf
 
 echo "[+] Successfully."
 echo "[!] You MUST reboot to apply the update."
