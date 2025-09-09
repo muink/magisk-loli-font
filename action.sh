d0f865b98b388c22b08cee47803fb038957c9168
Anya Lin <hukk1996@gmail.com>
2025-09-09 16:18:33 +0800

    v6.0

--- action.sh
+++ action.sh
@@ -2,9 +2,6 @@
 
 MODDIR=${0%/*}
 
-SOURCE='Loli'
-SANS_CJK='LoliCJK-Regular.ttf'
-
 sleep_pause() {
     # APatch and KernelSU needs this
     # but not KSU_NEXT, MMRL
@@ -13,18 +10,10 @@ sleep_pause() {
     fi
 }
 
-echo "[+] Repairing symlinks..."
-cd $MODDIR/system/fonts/
-for _t in *.placeholder; do
-    _v=${_t%.placeholder}
-    /system/bin/ln -sf ${SOURCE}-${_v#*-} ${_t}
-    mv ${_t} ${_v}
-done
-
 # Oneplus
 [ ! -f "/system/fonts/SysSans-En-Regular.ttf" ] || /system/bin/ln -sf ${SOURCE}-Regular.ttf SysSans-En-Regular.ttf
-[ ! -f "/system/fonts/SysSans-Hans-Regular.ttf" ] || /system/bin/ln -sf ${SANS_CJK} SysSans-Hans-Regular.ttf
-[ ! -f "/system/fonts/SysSans-Hant-Regular.ttf" ] || /system/bin/ln -sf ${SANS_CJK} SysSans-Hant-Regular.ttf
+#[ ! -f "/system/fonts/SysSans-Hans-Regular.ttf" ] || /system/bin/ln -sf ${SANS_CJK} SysSans-Hans-Regular.ttf
+#[ ! -f "/system/fonts/SysSans-Hant-Regular.ttf" ] || /system/bin/ln -sf ${SANS_CJK} SysSans-Hant-Regular.ttf
 
 echo "[+] Successfully."
 echo "[!] You MUST reboot to apply the update."
--- customize.sh
+++ customize.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+
 SKIPUNZIP=1
 
 
@@ -8,12 +8,8 @@ ui_print "*******************************"
 
 ui_print "- Extracting module files"
 unzip -o "$ZIPFILE" -x 'META-INF/*' fonts.tar.xz -d $MODPATH >&2
+. "$MODPATH/common_func.sh"
 # --------------------------------------------
-ui_print "- Searching in fonts.xml"
-#[[ -d /debug_ramdisk/.magisk/mirror ]] && MIRRORPATH=/debug_ramdisk/.magisk/mirror || unset MIRRORPATH
-FILEPATH=/system/etc/fonts.xml
-mkdir -p $MODPATH/$(dirname $FILEPATH) 2>/dev/null
-
 ui_print "- Unzipping font files..."
 FONTSPATH=/system/fonts
 unzip -oj "$ZIPFILE" fonts.tar.xz -d $TMPDIR >&2
@@ -22,33 +18,68 @@ tar -xf $TMPDIR/fonts.tar.xz -C $MODPATH/$FONTSPATH 2>/dev/null
 
 
 ui_print "- Installing fonts..."
-TARGET=$(sed -En '/<family name="sans-serif(-condensed)?">/,/<\/family>/ {s|.*<font [^\>]*>(.*).ttf.*|\1|p}' $FILEPATH | sort -u)
-SOURCE='Loli'
-# Just replace
-for _t in $TARGET; do
-  if [ -f "$MODPATH/$FONTSPATH/${SOURCE}-${_t#*-}.ttf" ]; then
-    ln -s ${SOURCE}-${_t#*-}.ttf $MODPATH/system/fonts/${_t}.ttf.placeholder
+# https://github.com/lxgw/simple-cjk-font-magisk-module-template/blob/main/system/etc/fonts.xml
+LEGACY_FONTS=/system/etc/fonts.xml
+FALLBACK_FONTS=/system/etc/font_fallback.xml
+SYSTEM_FONTS=/system/system_ext/etc/fonts_base.xml # Third-party
+
+FONT_SANSSERIF='Loli'
+FONT_SANSSERIF_STATIC='LoliStatic'
+FONT_CJK='LoliCJK'
+
+for _x in LEGACY_FONTS FALLBACK_FONTS SYSTEM_FONTS; do
+  eval "__x=\$$_x"
+  if [ -f "$__x" ]; then
+    mkdir -p $MODPATH/$(dirname $__x) 2>/dev/null
+    cp -af $__x $MODPATH/$__x 2>/dev/null
+
+    # Android 15+ https://source.android.com/docs/core/fonts/custom-font-fallback
+    if [ $API -gt 34 -a "$(basename $__x)" = "font_fallback.xml" ]; then # Android 15+ font_fallback.xml https://cs.android.com/android/platform/superproject/+/android15-release:frameworks/base/datfonts/font_fallback_cjkvf.xml
+      ui_print "- Installing Android 15 format $(basename $__x) ..."
+
+      # sans-serif(-condensed)?
+      sed -Ei "
+        s|^([ \t]*<family name=\"sans-serif\">)|\1$(format_VF "$FONT_SANSSERIF" 100)|;
+        s|^([ \t]*<family name=\"sans-serif-condensed\">)|\1$(format_VF "$FONT_SANSSERIF" 75)|;
+      " $MODPATH/$__x
+
+      # CJK
+      for _lang in zh-Hans zh-Hant,zh-Bopo ja ko; do
+        sed -Ei "
+          s|^([ \t]*<family lang=\""$_lang"\">)|\1$(format_CJKRegular "$FONT_CJK")|;
+        " $MODPATH/$__x
+      done
+    elif [ $API -gt 30 ]; then # Android 12+ https://android.googlesource.com/platform/frameworks/base/+/android12-release/data/fonts/fonts.xml
+      ui_print "- Installing Android 12 format $(basename $__x) ..."
+
+      # sans-serif(-condensed)?
+      sed -Ei "
+        s|^([ \t]*<family name=\"sans-serif\">)|\1$(format_9Weight "$FONT_SANSSERIF" 100)|;
+        s|^([ \t]*<family name=\"sans-serif-condensed\">)|\1$(format_9Weight "$FONT_SANSSERIF" 75)|;
+      " $MODPATH/$__x
+
+      # CJK
+      for _lang in zh-Hans zh-Hant,zh-Bopo ja ko; do
+        sed -Ei "
+          s|^([ \t]*<family lang=\""$_lang"\">)|\1$(format_CJKRegular "$FONT_CJK")|;
+        " $MODPATH/$__x
+      done
+    else # Android 11- https://android.googlesource.com/platform/frameworks/base/+/android11-release/data/fonts/fonts.xml
+      ui_print "- Installing Android 11 format $(basename $__x) ..."
+
+      # sans-serif(-condensed)?
+      sed -Ei "
+        s|^([ \t]*<family name=\"sans-serif(-condensed)?\">)|\1$(format_Static "$FONT_SANSSERIF" "$FONT_SANSSERIF_STATIC")|;
+      " $MODPATH/$__x
+
+      # CJK
+      for _lang in zh-Hans zh-Hant,zh-Bopo ja ko; do
+        sed -Ei "
+          s|^([ \t]*<family lang=\""$_lang"\">)|\1$(format_CJKRegular "$FONT_CJK")|;
+        " $MODPATH/$__x
+      done
+    fi
   fi
-done
-
-
-ui_print "- Installing CJK fonts..."
-# With fonts.xml
-# CJK=(zh-Hans zh-Hant,zh-Bopo ja ko)
-SANS_CJK='LoliCJK-Regular.ttf'
-#SANS_CJK='DFFangYuan-Std-W7.ttf'
-
-for _xml in $MIRRORPATH/$FILEPATH; do
-if [ -f "$_xml" ]; then
-  cp -af $_xml $MODPATH/${_xml/$MIRRORPATH/} 2>/dev/null
-
-  for _lang in zh-Hans zh-Hant,zh-Bopo ja ko; do
-    sed -Ei "/^[ \t]*<family lang=\"$_lang\">/,/^[ \t]*<\/family>/{ \
-      /fallback/b; \
-      s|^([ \t]*<font .* style=\"normal\")|\1>$SANS_CJK</font>\n\1|; \
-    }" $MODPATH/${_xml/$MIRRORPATH/}
-  done
-fi
 done
 
 
